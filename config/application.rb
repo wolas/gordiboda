38b09f4184e1ed17b8c23188e35f1cb1a8b6faa4
Jake <jcpenche@gmail.com>
2016-03-25 17:21:07 +0100

    more assets

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,6 @@ module Gordiboda
     config.active_record.raise_in_transactional_callbacks = true
 
     # For heroku to serve assets
-    config.serve_static_assets = true
+    config.serve_static_files = true
   end
 end
