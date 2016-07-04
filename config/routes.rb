4a8b098cf768beebe62b9792a60700396bf36cd6
Eli <e.morales.cinta@gmail.com>
2016-07-04 18:59:40 +0200

    Cleans app

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
 
-  resources :products, only: [:index]
-  resources :carts, only: [:show]
   devise_for :users
-  resources :order_items, only: [:create, :update, :destroy]
+
 
   get 'static/home'
   root "static#home"
+
+  resources :products, only: [:index]
+  resources :cart, only: [:show]
+  resources :order_items, only: [:create, :update, :destroy]
+
+
 end
