fffd77bfd7dbca938ccc5d2a540cf9e8e87dbd11
Eli <e.morales.cinta@gmail.com>
2016-07-07 18:22:46 +0200

    Add styles to admin panel

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,6 +1,6 @@
 class StaticController < ApplicationController
   def home
-    @products = Product.where(active: true)
+    @products = Product.all
     @order_item = current_order.order_items.new
   end
 end
