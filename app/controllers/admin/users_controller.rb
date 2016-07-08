60dce4d7bc56197499d532de48a268329dad358b
Eli <e.morales.cinta@gmail.com>
2016-07-08 11:51:26 +0200

    Fix user redirect after editing

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,45 +1,38 @@
 class Admin::UsersController < Admin::AdminController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
-  # GET /users
   def index
     @users = User.all
   end
 
-  # GET /users/1
   def show
   end
 
-  # GET /users/new
   def new
     @user = User.new
   end
 
-  # GET /users/1/edit
   def edit
   end
 
-  # POST /users
   def create
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to [:admin, @user], notice: 'User was successfully created.'
+      redirect_to admin_users_path
     else
       render action: 'new'
     end
   end
 
-  # PATCH/PUT /users/1
   def update
     if @user.update(user_params)
-      redirect_to [:admin, @user], notice: 'User was successfully updated.'
+      redirect_to admin_users_path
     else
       render action: 'edit'
     end
   end
 
-  # DELETE /users/1
   def destroy
     @user.destroy
     redirect_to admin_users_path
@@ -53,6 +46,6 @@ class Admin::UsersController < Admin::AdminController
 
     # Only allow a trusted parameter "white list" through.
     def user_params
-      params.require(:user).permit(:name, :email, :username, :password)
+      params.require(:user).permit(:name, :email, :password)
     end
 end
