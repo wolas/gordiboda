6a483608255edacdad713150015970a0e9072e4a
Eli <e.morales.cinta@gmail.com>
2016-07-10 11:32:14 +0200

    First step of fixing aws config for Paperclip

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -57,15 +57,15 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   #Sets Paperclip to upload images to Amazon S3
-  # config.paperclip_defaults = {
-#     storage: :s3,
-#     s3_credentials: {
-#       bucket: Rails.application.secrets.s3_bucket_name,
-#       s3_bucket_name: Rails.application.secrets.s3_bucket_name,
-#       access_key_id: Rails.application.secrets.access_key_id,
-#       secret_access_key: Rails.application.secrets.secret_access_key,
-#       s3_region: Rails.application.secrets.s3_region
-#     }
-#   }
+  config.paperclip_defaults = {
+    storage: :s3,
+    s3_credentials: {
+      bucket: Rails.application.secrets.s3_bucket_name,
+      s3_bucket_name: Rails.application.secrets.s3_bucket_name,
+      access_key_id: Rails.application.secrets.access_key_id,
+      secret_access_key: Rails.application.secrets.secret_access_key,
+      s3_region: Rails.application.secrets.s3_region
+    }
+  }
 
 end
