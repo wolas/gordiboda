fe31388a91cba757553205f38940953ce420ca18
Eli <e.morales.cinta@gmail.com>
2016-07-10 20:24:08 +0200

    Revert "First step of fixing aws config for Paperclip"
    This reverts commit 6a483608255edacdad713150015970a0e9072e4a.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,5 @@
 class Product < ActiveRecord::Base
   has_many :order_items
-  has_attached_file :picture, :s3_credentials => S3_CREDENTIALS, styles: { medium: "350x300>", thumb: "150x100>" }, default_url: ":style/missing.png"
+  has_attached_file :picture, styles: { medium: "350x300>", thumb: "150x100>" }, default_url: ":style/missing.png"
   validates_attachment_content_type :picture, content_type: /\Aimage\/.*\Z/
-
-
 end
