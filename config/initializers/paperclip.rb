e90d4733173c85262e2df2cb5b5cef5bd586d12a
Eli <e.morales.cinta@gmail.com>
2016-07-11 13:32:06 +0200

    Fixes Asset Pipeline

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -4,3 +4,10 @@ Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partitio
 Paperclip::Attachment.default_options[:s3_host_name] = 's3-eu-west-1.amazonaws.com'
 
 Paperclip::Attachment.default_options.merge!(use_timestamp: false)
+
+
+# if Rails.env.production?
+#   Paperclip.interpolates(:placeholder) do |attachment, style|
+#     ActionController::Base.helpers.asset_path("missing_#{style}.png")
+#   end
+# end
