28e382dfbfb3962979d78ed3be0de43111458a0b
Eli <e.morales.cinta@gmail.com>
2016-07-11 12:56:50 +0200

    dumb fix

--- config/application.rb
+++ config/application.rb
@@ -6,8 +6,6 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-config.serve_static_assets = true
-
 module Gordiboda
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
@@ -24,5 +22,6 @@ module Gordiboda
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.serve_static_assets = true
   end
 end
