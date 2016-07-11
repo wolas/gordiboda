f8f6e5af47363453f22f4e6cdbeba04ffaa6181d
Eli <e.morales.cinta@gmail.com>
2016-07-11 12:05:29 +0200

    Paperclip test

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,8 @@
 class Product < ActiveRecord::Base
   has_many :order_items
   has_attached_file :picture, styles: { medium: "350x300>", thumb: "150x100>" }, default_url: ":style/missing.png"
+
   validates_attachment_content_type :picture, content_type: /\Aimage\/.*\Z/
 end
+
+
