400674de603c0c4d5984485e1fafff9d660418f1
Eli <e.morales.cinta@gmail.com>
2016-07-12 12:38:12 +0200

    Add show more products

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,6 +1,7 @@
 class StaticController < ApplicationController
   def home
-    @products = Product.all.where("stock > ?", 0)
+    @featured_products = Product.all.where("stock > ?", 0).order(:stock).last(3)
+    @products = Product.all.where("stock > ?", 0) - @featured_products
     @order_item = current_order.order_items.new
   end
 end
