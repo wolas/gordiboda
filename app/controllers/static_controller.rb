0b0e1118981c1f6fa8d594f490f42eaa864f231a
Eli <e.morales.cinta@gmail.com>
2016-07-13 22:42:43 +0200

    Adds Filter and Clear buttons

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,15 +2,17 @@ class StaticController < ApplicationController
 
   def home
     @featured_products = Product.all.where("stock > ?", 0).order(:stock).last(3)
-    # @products = Product.all.where("stock > ?", 0) - @featured_products
     @products = Product.all
     @order_item = current_order.order_items.new
-    # @selected_products = Product.all.where("price < ?", params[:budget])
   end
 
   def search
     @query_string = params[:find]
-    @products = Product.all.where("price < ?", "#{@query_string}")
+    unless @query_string == ''
+      @products = Product.where("price <= ?", "#{@query_string}")
+    else
+      @products = Product.all
+    end
 
     render :home
   end
