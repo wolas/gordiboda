f6b0904eaa7643cb4e74395984fc9c17a8171304
Eli <e.morales.cinta@gmail.com>
2016-07-14 17:17:02 +0200

    Products only appear when stock > 0

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,17 +1,16 @@
 class StaticController < ApplicationController
 
   def home
-    @featured_products = Product.all.where("stock > ?", 0).order(:stock).last(3)
-    @products = Product.all
+    @products = Product.all.where("stock > ?", 0)
     @order_item = current_order.order_items.new
   end
 
   def search
     @query_string = params[:find]
     unless @query_string == ''
-      @products = Product.where("price <= ?", "#{@query_string}")
+      @products = Product.where("price <= ? AND stock > ?", "#{@query_string}", 0)
     else
-      @products = Product.all
+      @products = Product.all.where("stock > ?", 0)
     end
 
     render :home
